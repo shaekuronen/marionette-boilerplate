65b8c1992d55e598ce07e778c50f076a5523bcef
Shae Kuronen <shae.kuronen@gmail.com>
2014-03-13 06:06:19 -0700

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+/*
+ * marionette-boilerplate
+ * https://github.com/shaekuronen/marionette-boilerplate
+ *
+ * Copyright (c) 2014 Shae Kuronen
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+
+
+  });
+
+  // task
+  grunt.registerTask('', [], function () {
+
+    // load plugins for the task
+    grunt.loadNpmTasks('');
+
+    // execute the task
+    grunt.task.run(
+      ''
+    );
+
+  });
+  // end task
+
+};
