14b572676b6adab2040bcf3cfce88a6fb6df1f01
Shae Kuronen <shae@afanturs.com>
2014-04-14 17:04:09 -0700

    adding a connect server

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,15 +13,81 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
 
+  // start a node server
+  connect: {
+    preview: {
+      options: {
+        port: 9000,
+        keepalive: true,
+        base: './dev',
+        livereload: true,
+        // https://github.com/gruntjs/grunt-contrib-connect/issues/66
+        middleware: function(connect, options) {
+          grunt.log.debug('connect is ' + JSON.stringify(connect));
+          return [function(req, res) {
+            require('fs').createReadStream('dev/index.html').pipe(res);
+          }]
+        }
+      }
+    },
+    optimize: {
+      options: {
+        port: 9001,
+        keepalive: true,
+        base: './production',
+        // https://github.com/gruntjs/grunt-contrib-connect/issues/66
+        middleware: function(connect, options) {
+          return [function(req, res) {
+            require('fs').createReadStream('index.html').pipe(res);
+          }]
+        }
+      }
+    }
+  },
 
+    watch: {
+      dev: {
+        files: 'dev/**/*',
+        tasks: ['preview'],
+        options: {
+          debounceDelay: 250,
+          livereload: true
+        },
+      }
+    },
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'dev/js/site/**/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      }
+    },
 
   });
 
-  // task
-  grunt.registerTask('', [], function () {
+  // preview task
+  grunt.registerTask('preview', [], function () {
 
     // load plugins for the task
-    grunt.loadNpmTasks('');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // execute the task
+    grunt.task.run(
+      'connect:preview'
+    );
+
+  });
+  // end preview task
+
+  // task
+  grunt.registerTask('optimize', [], function () {
+
+    // load plugins for the task
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // execute the task
     grunt.task.run(
